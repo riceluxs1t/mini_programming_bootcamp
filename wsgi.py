b302e7e7fd2eb3f144c960d88121b5036429ed9c
riceluxs1t <riceluxs1t@gmail.com>
2016-12-15 01:46:50 -0600

    heroku fix?

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,18 @@
+"""
+WSGI config for website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
