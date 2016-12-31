f45c352ce93c730b7e3f4733cdcab3461dec6b3a
Jay Ryu <bluejay9676@gmail.com>
2017-01-01 04:56:49 +0900

    grader command in progress

--- grader/management/commands/create_homework.py
+++ grader/management/commands/create_homework.py
@@ -1,5 +1,7 @@
 from django.core.management import BaseCommand
-
+import boto3
+import logging
+from grader.config import HOMEWORK_NAME, USER_NAME
 
 """
 A command that runs and creates a homework and its necessary directories
@@ -8,24 +10,22 @@ A command that runs and creates a homework and its necessary directories
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(HOMEWORK_NAME, type=str)
+        parser.add_argument(USER_NAME, type=str)
+
     def handle(self, *args, **options):
-        """
-        :param args:
-        :param options:
-        :return: create hw directory to the bucket, and create python file
-        """
-        import boto3
-        import logging
+        homework_name = options.get(HOMEWORK_NAME)
+        user_name = options.get(USER_NAME)
 
         s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
                                  aws_secret_access_key="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")
 
-        # TODO : take python file dir as an arg and create python file in the bucket
         try:
             response = s3_client.put_object(
                 Bucket='rice-python-web-class',
-                Key='/homework/hw.py',
-                Body=open('submission.py','rb')
+                Key='/homework/' + user_name +'/' + homework_name,
+                Body=open('submission.py','rb') # TODO : specify how it will take in the python file
                 )
             logging.info("/homework/hw.py directory created  %s", response)
         except Exception as e:
--- grader/management/commands/download_submissions.py
+++ grader/management/commands/download_submissions.py
@@ -1,4 +1,5 @@
-
+import boto3
+from grader.config import HOMEWORK_NAME, USER_NAME
 from django.core.management import BaseCommand
 
 """
@@ -8,12 +9,15 @@ the source control platform
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(HOMEWORK_NAME, type=str)
+        parser.add_argument(USER_NAME, type=str)
 
     def handle(self, *args, **options):
+        # TODO :
+        homework_name = options.get(HOMEWORK_NAME)
+        user_name = options.get(USER_NAME)
 
-        # TODO: specify where to
-
-        import boto3
         s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
                                  aws_secret_access_key="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")
 
