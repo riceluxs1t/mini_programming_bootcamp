299be4530ff58d35f7afa2b04170ec8cfbdde77a
Jay Ryu <bluejay9676@gmail.com>
2017-01-01 05:03:43 +0900

    grader command in progress

--- grader/management/commands/download_submissions.py
+++ grader/management/commands/download_submissions.py
@@ -1,5 +1,4 @@
 import boto3
-from grader.config import HOMEWORK_NAME, USER_NAME
 from django.core.management import BaseCommand
 
 """
@@ -9,15 +8,8 @@ the source control platform
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument(HOMEWORK_NAME, type=str)
-        parser.add_argument(USER_NAME, type=str)
 
     def handle(self, *args, **options):
-        # TODO :
-        homework_name = options.get(HOMEWORK_NAME)
-        user_name = options.get(USER_NAME)
-
         s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
                                  aws_secret_access_key="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")
 
