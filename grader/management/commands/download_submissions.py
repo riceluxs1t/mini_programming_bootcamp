106967cd4840325888447dd1a27888c4539a0803
Jay Ryu <bluejay9676@gmail.com>
2017-01-01 03:19:57 +0900

    download_submission in progress

--- grader/management/commands/download_submissions.py
+++ grader/management/commands/download_submissions.py
@@ -1,4 +1,8 @@
+from boto3 import s3
 from django.core.management import BaseCommand
+
+import botocore
+
 
 
 """
@@ -11,4 +15,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # TODO: impelment this feature
-        pass
+        bucket = s3.Bucket('mybucket')
+        exists = True
+        try:
+            s3.meta.client.head_bucket(Bucket='mybucket')
+        except botocore.exceptions.ClientError as e:
+            # If a client error is thrown, then check that it was a 404 error.
+            # If it was a 404 error, then the bucket does not exist.
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404:
+                exists = False
