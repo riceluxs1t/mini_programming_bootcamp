756530cc70d0d4f4a9b777f9af31f1583b59d970
Jay Ryu <bluejay9676@gmail.com>
2017-01-01 04:49:07 +0900

    grader command in progress

--- grader/management/commands/create_homework.py
+++ grader/management/commands/create_homework.py
@@ -17,12 +17,16 @@ class Command(BaseCommand):
         import boto3
         import logging
 
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
+                                 aws_secret_access_key="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")
 
+        # TODO : take python file dir as an arg and create python file in the bucket
         try:
             response = s3_client.put_object(
                 Bucket='rice-python-web-class',
-                Key='/homework/hw.py')
-            logging.info("  %s", response)
+                Key='/homework/hw.py',
+                Body=open('submission.py','rb')
+                )
+            logging.info("/homework/hw.py directory created  %s", response)
         except Exception as e:
-            logging.warn("Bucket error %s", e)
+            logging.warn("Bucket not there :: error %s", e)
--- grader/management/commands/download_submissions.py
+++ grader/management/commands/download_submissions.py
@@ -10,16 +10,17 @@ the source control platform
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # TODO: impelment this feature
 
+        # TODO: specify where to
 
         import boto3
-        s3_client = boto3.client('s3')
+        s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
+                                 aws_secret_access_key="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")
+
         s3_client.download_file(
             Bucket='rice-python-web-class',
-                Key='homework/submission/',
-
-
-            'rice-python-web-class', 'hello-remote.txt', 'hello2.txt')
+            Key='/homework/hw.py',
+            Filename = 'hw.py'
+        )
 
 
