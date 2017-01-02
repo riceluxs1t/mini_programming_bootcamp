64387236e02c3770c2d00c4236a7280492673cb4
Jay Ryu <bluejay9676@gmail.com>
2017-01-02 12:30:43 +0900

    IP

--- grader/management/commands/create_homework.py
+++ grader/management/commands/create_homework.py
@@ -1,7 +1,11 @@
+import os
+
+import errno
 from django.core.management import BaseCommand
 import boto3
 import logging
-from grader.config import HOMEWORK_NAME, USER_NAME
+from grader.config import HOMEWORK_NAME
+from website.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_SUBMISSION_BUCKET_NAME
 
 """
 A command that runs and creates a homework and its necessary directories
@@ -12,27 +16,16 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(HOMEWORK_NAME, type=str)
-        parser.add_argument(USER_NAME, type=str)
-        parser.add_argument('hw_dir', type=str)
 
     def handle(self, *args, **options):
-        """
-        The format should be as such: python manage.py create_homework homework_name user_name hw_dir
-        for instance : python manage.py create_homework homework1.py jr51 /Users/Jay/homework/homework1.py
-        """
         homework_name = options.get(HOMEWORK_NAME)
-        user_name = options.get(USER_NAME)
-        hw_dir = options.get('hw_dir')
 
-        s3_client = boto3.client('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
-                                 aws_secret_access_key="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")
-
-        try:
-            response = s3_client.put_object(
-                Bucket='rice-python-web-class',
-                Key='/homework/' + user_name + '/' + homework_name,
-                Body=open(hw_dir,'rb')
-                )
-            logging.info("/homework/hw.py directory created  %s", response)
-        except Exception as e:
-            logging.warn("Bucket not there :: error %s", e)
+        filename = "grader/submissions/" + homework_name + "/__init__.py"
+        if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+        with open(filename, "w") as f:
+            f.write("")
--- grader/management/commands/download_submissions.py
+++ grader/management/commands/download_submissions.py
@@ -1,5 +1,7 @@
 import boto3
 from django.core.management import BaseCommand
+from website.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_SUBMISSION_BUCKET_NAME
+
 
 """
 A command that runs and downloads homework submissions from
@@ -12,13 +14,13 @@ class Command(BaseCommand):
     # TODO : each 'bucket/homework/user_name/homework_name'
 
     def handle(self, *args, **options):
-        s3_client = boto3.resource('s3', aws_access_key_id="AKIAI5LREICCGFBFLJMQ",
-                                 aws_secret_access_key="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")
+        s3_client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-        my_bucket = s3_client.Bucket('rice-python-web-class').get
+        my_bucket = s3_client.Bucket(S3_SUBMISSION_BUCKET_NAME)
 
         for object in my_bucket.objects.all():
-            print(object)
+            print(object.key)
 
         # s3_client.download_file(
         #     Bucket='rice-python-web-class',
