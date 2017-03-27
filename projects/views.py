af43f352bf6408b29d97431c5cfbf49f47e18c7e
riceluxs1t <riceluxs1t@gmail.com>
2017-03-27 16:02:06 -0500

    face picture added upon call

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
 import boto3
 import json
+import requests
+import cStringIO
+
+from PIL import Image, ImageDraw
+from io import BytesIO
 
 from django.http import *
 from django.shortcuts import render
@@ -13,7 +18,7 @@ def analyze_image(request):
                              aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
     s3_response = s3_client.list_objects(Bucket=S3_PROJECT_IMAGE_BUCKET_NAME)
-    image_names = [content['Key'] for content in s3_response['Contents']]
+    image_names = [content['Key'] for content in s3_response['Contents'] if 'examples' in content['Key']]
 
     return render(
         request,
@@ -52,8 +57,47 @@ def call_face_api(request):
                 'ALL',
             ]
         )
+        print face_api_response
+        # TODO: replace the hard coding
+        response = requests.get(
+            "https://s3.amazonaws.com/rice-python-project-images/" + image_name_selected
+        )
+        img = Image.open(BytesIO(response.content))
+        width, height = img.size
 
-        return HttpResponse(json.dumps({"status": True, "body": face_api_response}), content_type="application/json")
+        for faceDetail in face_api_response["FaceDetails"]:
+
+            bounding_rectangle = faceDetail["BoundingBox"]
+            bounding_width = bounding_rectangle["Width"]
+            bounding_height = bounding_rectangle["Height"]
+            bounding_top = bounding_rectangle["Top"]
+            bounding_left = bounding_rectangle["Left"]
+
+            draw = ImageDraw.Draw(img)
+            draw.rectangle(
+                (
+                    width*bounding_left,
+                    height*bounding_top,
+                    width*(bounding_left + bounding_width),
+                    height*(bounding_top+bounding_height)
+
+                ), outline=(173, 255, 47))
+
+        s3 = boto3.resource('s3')
+        output_image = cStringIO.StringIO()
+        img.save(output_image, "jpeg")
+
+        filepath = "processed/" + image_name_selected[10:]
+        s3.Bucket(S3_PROJECT_IMAGE_BUCKET_NAME).put_object(
+            Key=filepath,
+            Body=output_image.getvalue(),
+            ACL="public-read"
+        )
+
+        return HttpResponse(
+            json.dumps({"status": True, "body": face_api_response, "link": "https://s3.amazonaws.com/rice-python-project-images/" + filepath}),
+            content_type="application/json"
+        )
     except:
         raise
 
@@ -65,14 +109,14 @@ def upload_file(request):
         # file path should be examples/FILE_NAME
         filepath = "examples/" + image_uploaded.name
 
-        s3 = boto3.client(
-            's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        s3 = boto3.resource('s3')
+        # upload object
+        s3.Bucket(S3_PROJECT_IMAGE_BUCKET_NAME).put_object(
+            Key=filepath,
+            Body=image_uploaded.read(),
+            ACL="public-read"
         )
 
-        # upload object
-        s3.upload_fileobj(Fileobj=image_uploaded, Bucket=S3_PROJECT_IMAGE_BUCKET_NAME, Key=filepath)
         return HttpResponse(json.dumps({"result": True}), content_type="application/json")
     except:
         raise
